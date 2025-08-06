85a35ed3907b8cbf2359bc3320277366e8d50742
Baguette <82762193+HDevv@users.noreply.github.com>
2025-08-06 15:25:22 +0200

    first

---
+++ assets/shop/entrypoint.js
@@ -0,0 +1,7 @@
+import '@vendor/sylius/mollie-plugin/assets/shop/entrypoint';
+import './bootstrap.js';
+
+// In this file you can import assets like images or stylesheets
+console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
+
+import '@vendor/sylius/cms-plugin/assets/shop/entrypoint';
